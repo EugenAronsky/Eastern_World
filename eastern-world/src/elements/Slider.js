d5cb26368a5174aff92727c88f203255e829cdaa
EugenAronsky <zolotareveugen.work@gmail.com>
2022-07-06 19:00:05 +0200

    Change Slider

--- eastern-world/src/elements/Slider.js
+++ eastern-world/src/elements/Slider.js
@@ -2,20 +2,53 @@ import React from 'react';
 import "./slider.css"
 import SliderCard from "./SliderCard.js"
 
+let time = 7000;
+
 class Slider extends React.Component{
 
     constructor(){
         super();
-        this.state = {slider : 1};
+        this.state = {
+            slider : 1,
+            slidMeRight : true,
+            sliderState : false,
+            clik : false,
+        };
     }
 
     chengeSlide = (slideNumber) =>{
         this.setState({slider : slideNumber});
     };
 
+    timer = () => {
+        if(this.state.slidMeRight){
+            if(this.state.slider === 3) {
+                this.setState({slidMeRight : false})
+                this.setState({slider : this.state.slider - 1});
+            }
+            else{
+                this.setState({slider : this.state.slider + 1});
+            }
+        }
+        else if(!this.state.slidMeRight){
+            if(this.state.slider === 1){
+                this.setState({slidMeRight : true});
+                this.setState({slider : this.state.slider + 1});
+            } 
+            else{
+                this.setState({slider : this.state.slider - 1});
+            }
+        }
+    }
+
     render() {
+        if(!this.state.sliderState){
+            this.setState({sliderState : true});
+            setTimeout( () => this.timer(), time);
+        } 
+
         return(
-            <div className="SliderBox">
+            <div className={this.state.slider === 1 ? "SliderBox img1" : this.state.slider === 2 ? "SliderBox img2" : "SliderBox img3"}>
                 <div className="selection">
                         <div onClick={() => this.chengeSlide(1)} className ={this.state.slider === 1 ? "selection-active" : "selection-not-active"}></div>
                         <div onClick={() => this.chengeSlide(2)} className ={this.state.slider === 2 ? "selection-active" : "selection-not-active"}></div>
@@ -31,5 +64,4 @@ class Slider extends React.Component{
         );
     }
 }
-
 export default Slider;
